ad202b86137ea25ab96fc466f4a4d72f6a1ddc23
Javier Salcedo <javiersalcedo.p@gmail.com>
2018-09-01 05:45:57 +0200

    QuestMark and Italian Menu Translation

--- Assets/Scripts/Controllers/GJ_PlayerController.cs
+++ Assets/Scripts/Controllers/GJ_PlayerController.cs
@@ -1,43 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+namespace GameJam.Player{
+	public class GJ_PlayerController : GJ_SingletonMonobehaviour<GJ_PlayerController> {
 
-public class GJ_PlayerController : MonoBehaviour {
+		private Vector3 m_horDir, m_verDir, m_movDir;
+		[SerializeField] private float m_speed = 10.0f;
+		[SerializeField] private float m_jumpForce = 500.0f;
+		private Rigidbody m_rb;
+		private bool m_isOnFloor;
 
-	private Vector3 m_horDir, m_verDir, m_movDir;
-	[SerializeField] private float m_speed = 10.0f;
-	[SerializeField] private float m_jumpForce = 500.0f;
-	private Rigidbody m_rb;
-	private bool m_isOnFloor;
-
-	// Use this for initialization
-	void Start () {
-		m_rb = GetComponent<Rigidbody>(); 
-		m_isOnFloor = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-		// Isometric 45º movement
-		m_horDir = new Vector3(Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Horizontal"));
-		m_verDir = new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Vertical"));
-		m_movDir = m_horDir + m_verDir;
-        
-		if (Input.GetKeyDown("space") && m_isOnFloor) {
-			m_rb.AddForce(transform.up * m_jumpForce, ForceMode.Impulse);
+		// Use this for initialization
+		void Start () {
+			m_rb = GetComponent<Rigidbody>(); 
 			m_isOnFloor = false;
 		}
+		
+		// Update is called once per frame
+		void Update () {
 
-		transform.position += m_movDir * m_speed * Time.deltaTime;
-		//m_rb.AddForce(m_movDir*m_speed*Time.deltaTime, ForceMode.Impulse);
-	}
+			// Isometric 45º movement
+			m_horDir = new Vector3(Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Horizontal"));
+			m_verDir = new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Vertical"));
+			m_movDir = m_horDir + m_verDir;
+			
+			if (Input.GetKeyDown("space") && m_isOnFloor) {
+				m_rb.AddForce(transform.up * m_jumpForce, ForceMode.Impulse);
+				m_isOnFloor = false;
+			}
 
-	void OnCollisionEnter (Collision hit) {
+			transform.position += m_movDir * m_speed * Time.deltaTime;
+			//m_rb.AddForce(m_movDir*m_speed*Time.deltaTime, ForceMode.Impulse);
+		}
 
-		Debug.LogWarning(hit.collider.tag);
-		if(hit.collider.tag == "Surface") {
-			m_isOnFloor = true;
+		void OnCollisionEnter (Collision hit) {
+
+			Debug.LogWarning(hit.collider.tag);
+			if(hit.collider.tag == "Surface") {
+				m_isOnFloor = true;
+			}
 		}
 	}
+
 }
---
+++ Assets/Scripts/Miscellaneous/GJ_SimpleHover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GJ_SimpleHover : MonoBehaviour {
+
+	[SerializeField] private float m_freq;
+	[SerializeField] private float m_height;
+	private float displacement;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		displacement = m_height * Mathf.Sin(Time.time * m_freq);
+		transform.position += new Vector3(0.0f, displacement, 0.0f);
+	}
+}
